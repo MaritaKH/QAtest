77fb17abbd9a90b9fce443f6c126c0afdcd791af
marita khinchigashvili <maritakhinchigashvili@gmail.com>
2024-03-12 08:35:03 +0100

    add text to Readme

---
+++ script.js
@@ -0,0 +1,21 @@
+let a=23, b=4;
+console.log(a+b)
+console.log(a*b)
+let c=true;
+console.log(c)
+
+let person = {
+    name:"shaqti",
+    lastname:"kundalini",
+    age:36,
+    phone:347283927,
+    email:"shakti@gmail.com",
+    address: {
+        "city":"NY",
+        "Country":"USA"
+    },
+    friends:["eka", "meri", "natu", "sopo","tina"]
+
+}
+    
+console.log(person.name, person.friends[1])
